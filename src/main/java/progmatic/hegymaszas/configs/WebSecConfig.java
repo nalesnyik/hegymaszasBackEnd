a70a825d7e297297001eee048922233f2de18518
progmatic-4 <harnorby@gmail.com>
2020-02-04 13:57:30 +0100

    registration

---
+++ src/main/java/progmatic/hegymaszas/configs/WebSecConfig.java
@@ -0,0 +1,34 @@
+package progmatic.hegymaszas.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@EnableWebSecurity
+public class WebSecConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        http.csrf().disable()
+                .formLogin()
+                //.loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/loginpage/beforelogin", "/home", "/", "/registerUser", "/register").permitAll()
+                .antMatchers("/users", "/user/changeRole").hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+    }
