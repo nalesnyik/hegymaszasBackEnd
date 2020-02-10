0fcdc5112f3e844a611600a0e944dfa3a9edafa5
nalesnyik <naleslg@gmail.com>
2020-02-10 13:45:57 +0100

    fantörpikus

--- src/main/java/progmatic/hegymaszas/controllers/UserController.java
+++ src/main/java/progmatic/hegymaszas/controllers/UserController.java
@@ -1,28 +1,32 @@
 package progmatic.hegymaszas.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import progmatic.hegymaszas.dto.MyUserDto;
 import progmatic.hegymaszas.modell.Route;
 import progmatic.hegymaszas.services.UserService;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
     private UserService userService;
 
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+
     @PostMapping(path = "/register")
     public void register(@Valid @RequestBody MyUserDto user) {
         userService.createUser(user);
     }
+
 
     @PostMapping(path = "/filter")
     public void filter(@RequestBody String grade, String name, int rating, int height, String climbingPlaceName) {
