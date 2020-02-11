0db976fd1e9e332f26b93b70cffbb06ad3295058
progmatic-4 <harnorby@gmail.com>
2020-02-11 09:59:19 +0100

    filter almost done...

--- src/main/java/progmatic/hegymaszas/controllers/UserController.java
+++ src/main/java/progmatic/hegymaszas/controllers/UserController.java
@@ -3,12 +3,8 @@ package progmatic.hegymaszas.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import progmatic.hegymaszas.dto.MyUserDto;
-import progmatic.hegymaszas.modell.Route;
 import progmatic.hegymaszas.services.UserService;
-
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class UserController {
