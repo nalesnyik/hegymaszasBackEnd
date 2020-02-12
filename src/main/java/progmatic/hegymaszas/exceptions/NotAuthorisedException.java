b1162056022d603ac31a50690de92da74d36ed08
uvegesi <uveges.istvan@gmail.com>
2020-02-12 13:59:28 +0100

    route verification

--- src/main/java/progmatic/hegymaszas/exceptions/NotAuthorisedException.java
+++
@@ -1,8 +0,0 @@
-package progmatic.hegymaszas.exceptions;
-
-public class NotAuthorisedException extends Exception {
-
-    public NotAuthorisedException() {}
-
-    public NotAuthorisedException(String message) { super(message);}
-}
